4c2ba759e79013628ae4c585b586e2c5d1786e56
Ravena <ravena.carneiro@gmail.com>
2022-04-14 09:57:34 -0300

    Aula Base Variaveis

---
+++ src/diocursobase/aulavariaveisbase.java
@@ -0,0 +1,52 @@
+package diocursobase;
+
+public class aulavariaveisbase {
+        public static void main(String[] args) {
+
+            int i;
+            //int i;
+            int I;
+            int _1a;
+            int $aq;
+
+            I = 10;
+            i = 5;
+            _1a = 20;
+            $aq = 7;
+
+            final int j = 10;
+            //j = 15
+            int arsn24678md;
+            //int arsn246 78md;
+            int arsn24678_md = 10;
+            //int arsn24%678_md = 10;
+
+            arsn24678md = 100;
+            arsn24678_md = 10;
+
+
+             int quantidadeProduto = 50;
+             //int QuantidadeProduto;
+             final int NUMERO_TENTATIVAS = 5;
+             //final int numeroTentativas = 5;
+             int QUANTIDADE_OPCOES = 25;
+             //int qtdProd;
+
+
+            System.out.println(i);
+            System.out.println(I);
+            System.out.println(_1a);
+            System.out.println(arsn24678_md);
+            System.out.println(NUMERO_TENTATIVAS);
+            System.out.println(QUANTIDADE_OPCOES);
+            System.out.println(j);
+            System.out.println($aq);
+            System.out.println(arsn24678md);
+            System.out.println(quantidadeProduto);
+
+
+
+
+
+    }
+}
---
+++ src/diocursobase/tiposdevariaveis.java
@@ -0,0 +1,71 @@
+package diocursobase;
+
+public class tiposdevariaveis {
+    public static void main(String[] args) {
+        //Tipos de Dados Númericos
+        byte b1 = 10;
+        byte b2 = 20;
+
+
+        short s1 = 20000;
+        //short s2 = 40000;
+
+
+        //int i1 = -10000000000;
+        int i2 = 28500;
+
+        long l1 = 100000000000000L;
+        long l2 = 200450006000555L;
+
+        //float f1 = 4.5;
+        float f2 = 10.68f;
+
+
+        double d1 = 85.69;
+        double d2 = 99.84d;
+
+
+        //Tipos de Dados Textuais
+
+        char c1 = 'R';
+        //char c2 = 'RD';
+        char c3 = '\u0057';
+
+        String st1 = "Ravena";
+        String st2 = "Dafne";
+        String st3 = "dffsef fefefaefe fefefe effefefefe ";
+
+
+        String dt1 = "09/06/2022";
+
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(s1);
+        System.out.println(i2);
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(c1);
+        System.out.println(c3);
+        System.out.println(st1);
+        System.out.println(st2);
+        System.out.println(st3);
+        System.out.println(dt1);
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+
+
+
+
+
+
+
+    }
+}
