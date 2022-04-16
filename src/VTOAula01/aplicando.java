e678cf5ae77d5b2f64f51e1149ddbe19a26b9ea1
Ravena <ravena.carneiro@gmail.com>
2022-04-16 20:00:50 -0300

    Aula Base Variaveis

---
+++ src/VTOAula01/aplicando.java
@@ -0,0 +1,21 @@
+package VTOAula01;
+
+public class aplicando {
+    public static void main(String[] args) {
+        //Calculadora
+
+        System.out.println("Exercicio Calculadora");
+        VTOAula01.Calculadora.soma(3, 6);
+        VTOAula01.Calculadora.subtracao(2, 6);
+        VTOAula01.Calculadora.multiplicacao(7, 8);
+        VTOAula01.Calculadora.divisao(5, 2.5);
+
+        // Mensagem
+        System.out.println("Exercicio Mensagem");
+        VTOAula01.Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem( 16);
+        Mensagem.obterMensagem( 1);
+
+
+    }
+}
