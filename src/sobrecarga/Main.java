0e7ad068738608e8dc9b8ee65591eb349c9db7f7
Ravena <ravena.carneiro@gmail.com>
2022-04-17 19:02:42 -0300

    Aula Base Variaveis: Métodos e atributos

--- src/sobrecarga/Main.java
+++ src/sobrecarga/Main.java
@@ -7,10 +7,10 @@ public class Main {
         // Quadrilátero
         System.out.println("Exercicio Quadrilatero");
         Quadrilatero.area(3);
-        Quadrilatero.area(5, 5);
+        Quadrilatero.area(5d, 5d);
         Quadrilatero.area( 8, 7, 9);
-        float diagonal1 = 8;
-        float diagonal2 = 10;
+        float diagonal1 = 5f;
+        float diagonal2 = 5f;
         Quadrilatero.area(diagonal1, diagonal2);
     }
 }
